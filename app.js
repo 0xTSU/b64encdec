12c89ab3461fb8775ac3a586060751eb869ed5d4
zxtsubxu <mccodi11@gmail.com>
2022-04-17 05:06:33 -0700

    1.0

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ var clearbtn = document.getElementById('btn-clear')
 var encodebtn = document.getElementById('btn-encode')
 var decodebtn = document.getElementById('btn-decode')
 
-const b64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
-
 clearbtn.addEventListener('click', function(e) {
     document.getElementById('original-text').value = ''
     document.getElementById('b64-text').value = ''
